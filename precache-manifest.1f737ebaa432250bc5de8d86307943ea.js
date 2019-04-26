0b66bf3bada9fbbb0e52cae8ca93ddb005b591d3
Gustavo Sampaio <gbritosampaio@gmail.com>
2019-04-26 11:30:07 -0300

    Updates

---
+++ precache-manifest.1f737ebaa432250bc5de8d86307943ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c5b8843c235b03e7482ac5b54fcc69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac48ffad2e7047e991d1",
+    "url": "/static/css/2.aa095914.chunk.css"
+  },
+  {
+    "revision": "fca9c4ac6e06ab133c5a",
+    "url": "/static/css/main.4d82fe8f.chunk.css"
+  },
+  {
+    "revision": "ac48ffad2e7047e991d1",
+    "url": "/static/js/2.3e876ac1.chunk.js"
+  },
+  {
+    "revision": "fca9c4ac6e06ab133c5a",
+    "url": "/static/js/main.81fa2ad4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
