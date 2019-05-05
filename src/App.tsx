68ca64ebeb0864c801d5a968f103165778663dcc
Gustavo Sampaio <gbritosampaio@gmail.com>
2019-05-05 11:59:20 -0300

    Add project tags
    Signed-off-by: Gustavo Sampaio <gbritosampaio@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faTwitterSquare, faTelegram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faTwitterSquare, faTelegram, faMedium } from '@fortawesome/free-brands-svg-icons'
 import { faAddressBook, faChevronCircleDown, faEnvelope, faProjectDiagram } from '@fortawesome/free-solid-svg-icons'
 import { Github } from 'react-social-github';
 
@@ -23,9 +23,28 @@ const CreateContactButtons: React.FC = () => {
       <a href="https://t.me/GustavoKatel" target="_blank">
         <FontAwesomeIcon icon={faTelegram} />
       </a>
+      <a href="https://medium.com/@gustavokatel" target="_blank">
+        <FontAwesomeIcon icon={faMedium} />
+      </a>
     </div>
   );
 }
+
+interface TagsProps {
+  tags: string[];
+}
+
+const Tags: React.FC<TagsProps> = (props) => {
+  const tags = props.tags || [];
+
+  return <div className="tags">
+    {
+      tags.map((tag, i) => {
+        return <div key={i} className="tag">{tag}</div>
+      })
+    }
+  </div>
+};
 
 const App: React.FC = () => {
   const contactButtons = CreateContactButtons({});
@@ -51,23 +70,59 @@ const App: React.FC = () => {
         </div>
 
         <div className="other-cards">
-          <Card icon={<FontAwesomeIcon icon={faProjectDiagram} />}>
+          <Card
+            icon={<FontAwesomeIcon icon={faProjectDiagram} />}
+            footer={
+              <Tags tags={[
+                'golang', 'react', 'electron',
+                'nodejs', 'rabbitmq', 'typescript', 'grpc'
+              ]} />
+            }
+          >
             <Github type="widget" tooltipOnHover={true} user="runletapp" repo="runlet" ></Github>
           </Card>
 
-          <Card icon={<FontAwesomeIcon icon={faProjectDiagram} />}>
+          <Card
+            icon={<FontAwesomeIcon icon={faProjectDiagram} />}
+            footer={
+              <Tags tags={[
+                'c++', 'python', 'pybind', 'ModSecurity', 'pytest', 'TDD', 'conda'
+              ]} />
+            }
+          >
             <Github type="widget" tooltipOnHover={true} user="actions-security" repo="pymodsecurity" ></Github>
           </Card>
 
-          <Card icon={<FontAwesomeIcon icon={faProjectDiagram} />}>
+          <Card
+            icon={<FontAwesomeIcon icon={faProjectDiagram} />}
+            footer={
+              <Tags tags={[
+                'python', 'django', 'ModSecurity', 'pytest', 'TDD', 'conda'
+              ]} />
+            }
+          >
             <Github type="widget" tooltipOnHover={true} user="GustavoKatel" repo="django-pymodsecurity" ></Github>
           </Card>
 
-          <Card icon={<FontAwesomeIcon icon={faProjectDiagram} />}>
+          <Card
+            icon={<FontAwesomeIcon icon={faProjectDiagram} />}
+            footer={
+              <Tags tags={[
+                'python', 'pushbullet api', 'pytest'
+              ]} />
+            }
+          >
             <Github type="widget" tooltipOnHover={true} user="GustavoKatel" repo="pushbullet-cli" ></Github>
           </Card>
 
-          <Card icon={<FontAwesomeIcon icon={faProjectDiagram} />}>
+          <Card
+            icon={<FontAwesomeIcon icon={faProjectDiagram} />}
+            footer={
+              <Tags tags={[
+                'c++', 'raytracing', 'qt'
+              ]} />
+            }
+          >
             <Github type="widget" tooltipOnHover={true} user="TecRen-UFPB" repo="RayTracerQt" ></Github>
           </Card>
         </div>
